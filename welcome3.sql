7a043a7551667c5cdb430a1b2efa2e83f469e7ef
fidhafathima123 <144669671+fidhafathima123@users.noreply.github.com>
2024-04-08 15:18:42 +0530

    Add files via upload

---
+++ welcome3.sql
@@ -0,0 +1,19 @@
+ CREATE OR REPLACE FUNCTION welcome_msg_func ( p_name IN
+VARCHAR2) RETURN VARCHAR2
+IS
+BEGIN
+RETURN ('Welcome '|| p_name);
+END;
+/
+ DECLARE
+lv_msg VARCHAR2(250);
+BEGIN
+lv_msg:=welcome_msg_func('Amrutha');
+dbms_output.put_line(lv_msg);
+END;
+ /
+
+
+
+ SELECT welcome_msg_func('Amrutha') FROM DUAL;
+WELCOME_MSG_FUNC('Amrutha')
